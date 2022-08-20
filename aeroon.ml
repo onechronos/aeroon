5ab9e7b0fda19e90a45b5fd6e02c3dfc6cb2d0f2
Mika Illouz <mika@illouz.net>
2022-08-19 17:11:11 -0700

    modularizing context struct

--- aeroon.ml
+++ aeroon.ml
@@ -1,30 +1,46 @@
 open Ctypes
 open Foreign
 
+
+module Context = struct
+  let t : [`Context] abstract typ =
+    abstract ~name:"aeron_context_stct" ~size:1 ~alignment:1
+
+  let init from =
+    foreign "aeron_context_init" (ptr (ptr t) @-> returning int) ~from
+
+  let get_dir from =
+    foreign "aeron_context_get_dir" ((ptr t) @-> returning string) ~from
+
+  let alloc () = allocate (ptr t) (from_voidp t null)
+
+  type o = {
+    init : [ `Context ] abstract Ctypes_static.ptr Ctypes_static.ptr -> int;
+    get_dir : [ `Context ] abstract Ctypes_static.ptr -> string;
+    alloc : unit -> [ `Context ] abstract Ctypes_static.ptr ptr
+  }
+
+  let create from = {
+    init = init from;
+    get_dir = get_dir from;
+    alloc;
+  }
+
+end
+
 let _ =
   let lib_so_path = Sys.argv.(1) in
   let lib = Dl.(dlopen ~filename:lib_so_path ~flags:[RTLD_NOW]) in
 
-  let context_t : [`Aeron_context] abstract typ =
-    abstract ~name:"aeron_context_stct" ~size:1 ~alignment:1
-  in
+  let c = Context.create lib in
 
-  let context_init =
-    foreign "aeron_context_init" (ptr (ptr context_t) @-> returning int) ~from:lib
-  in
-
-  let context_get_dir =
-    foreign "aeron_context_get_dir" ((ptr context_t) @-> returning string) ~from:lib
-  in
-
-  let p_context = allocate (ptr context_t) (from_voidp context_t null) in
-
-  let err = context_init p_context in
+  let p_context = c.alloc () in
+  let err = c.init p_context in
   match err with
   | -1 -> failwith "context_init"
   | 0 ->
      let p = !@ p_context in
-     let dir = context_get_dir p in
+     let dir = c.get_dir p in
      Printf.printf "dir=%s\n" dir
 
   | _ -> assert false
