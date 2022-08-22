9afaa1624a3972d657d54075be8fd43d5f20f2e1
Mika Illouz <mika@illouz.net>
2022-08-22 11:18:54 -0700

    using static (rather than dynamic) linking, to eliminate need for omnipresent ~from library source parameter

--- aeroon.ml
+++ aeroon.ml
@@ -1,49 +1,29 @@
 open Ctypes
 open Foreign
 
-
 module Context = struct
   let t : [`Context] abstract typ =
     abstract ~name:"aeron_context_stct" ~size:1 ~alignment:1
 
-  let init from =
-    foreign "aeron_context_init" (ptr (ptr t) @-> returning int) ~from
+  let init =
+    foreign "aeron_context_init" (ptr (ptr t) @-> returning int)
 
-  let get_dir from =
-    foreign "aeron_context_get_dir" ((ptr t) @-> returning string) ~from
+  let get_dir =
+    foreign "aeron_context_get_dir" ((ptr t) @-> returning string)
 
   let alloc () = allocate (ptr t) (from_voidp t null)
-
-  type o = {
-    init : [ `Context ] abstract Ctypes_static.ptr Ctypes_static.ptr -> int;
-    get_dir : [ `Context ] abstract Ctypes_static.ptr -> string;
-    alloc : unit -> [ `Context ] abstract Ctypes_static.ptr ptr
-  }
-
-  let create from = {
-    init = init from;
-    get_dir = get_dir from;
-    alloc;
-  }
 
 end
 
 let _ =
-  let lib_so_path = Sys.argv.(1) in
-  let lib = Dl.(dlopen ~filename:lib_so_path ~flags:[RTLD_NOW]) in
-
-  let c = Context.create lib in
-
-  let p_context = c.alloc () in
-  let err = c.init p_context in
+  let p_context = Context.alloc () in
+  let err = Context.init p_context in
   match err with
   | -1 -> failwith "context_init"
   | 0 ->
      let p = !@ p_context in
-     let dir = c.get_dir p in
+     let dir = Context.get_dir p in
      Printf.printf "dir=%s\n" dir
 
   | _ -> assert false
-
-
 
