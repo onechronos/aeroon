deb9dbb3f4930a208deb8d5567333d1688943e57
Simon Cruanes <simon.cruanes.2007@m4x.org>
2022-08-30 16:48:18 -0400

    wip: start high level API

--- lib/aeroon.ml
+++ lib/aeroon.ml
@@ -1,1 +1,62 @@
 module Bindings = Aeroon_bindings
+
+open struct
+  module Ty = Bindings.C.Type
+  module F = Bindings.C.Functions
+  module Alloc = Bindings.Alloc
+
+  type 'a aptr = 'a Ctypes_static.abstract Ctypes_static.ptr
+
+  let is_null = Ctypes.is_null
+
+  let ( !@ ) = Ctypes.( !@ )
+end
+
+module Context = struct
+  type t = [ `Context ] aptr
+
+  let create () : t =
+    let ptr =
+      Alloc.ptr_context
+        ~finalise:(fun ctx ->
+          if not (is_null ctx) then ignore (F.context_close !@ctx : int))
+        ()
+    in
+    if not (is_null ptr) then (
+      let err = F.context_init ptr in
+      if err <> 0 then failwith "aeron.context";
+      (* TODO: error code *)
+      !@ptr
+    ) else
+      failwith "aeron.context"
+
+  let get_dir (self : t) : string = F.context_get_dir self
+
+  let set_dir (self : t) dir : unit =
+    ignore (F.context_set_dir self dir : _ option)
+
+  let get_driver_timeout_ms (self : t) : int64 =
+    F.context_get_driver_timeout_ms self |> Unsigned.UInt64.to_int64
+
+  let set_driver_timeout_ms (self : t) (n : int64) =
+    F.context_set_driver_timeout_ms self (Unsigned.UInt64.of_int64 n)
+
+  (* TODO: more config *)
+
+  (* TODO
+     let on_new_subscription (self:t)
+  *)
+end
+
+module Client = struct
+  type t = [ `Client ] aptr
+end
+
+(** Version of Aeron *)
+module Version = struct
+  let major = F.version_major ()
+
+  let minor = F.version_minor ()
+
+  let patch = F.version_patch ()
+end
--- lib/bindings/alloc.ml
+++ lib/bindings/alloc.ml
@@ -2,17 +2,27 @@ open Ctypes
 open C.Type
 
 open struct
-  let a t =
-    allocate (ptr t) (from_voidp t null)
+  let a ?finalise t = allocate ?finalise (ptr t) (from_voidp t null)
 end
 
-let ptr_context () = a context
-let ptr_client () = a client
-let ptr_publication () = a publication
-let ptr_exclusive_publication () = a exclusive_publication
-let ptr_subscription () = a subscription
-let ptr_fragment_assembler () = a fragment_assembler
-let ptr_image_fragment_assembler () = a image_fragment_assembler
-let ptr_async_add_subscription () = a async_add_subscription
-let ptr_async_add_publication () = a async_add_publication
-let ptr_async_add_exclusive_publication () = a async_add_exclusive_publication
+let ptr_context ?finalise () = a ?finalise context
+
+let ptr_client ?finalise () = a ?finalise client
+
+let ptr_publication ?finalise () = a ?finalise publication
+
+let ptr_exclusive_publication ?finalise () = a ?finalise exclusive_publication
+
+let ptr_subscription ?finalise () = a ?finalise subscription
+
+let ptr_fragment_assembler ?finalise () = a ?finalise fragment_assembler
+
+let ptr_image_fragment_assembler ?finalise () =
+  a ?finalise image_fragment_assembler
+
+let ptr_async_add_subscription ?finalise () = a ?finalise async_add_subscription
+
+let ptr_async_add_publication ?finalise () = a ?finalise async_add_publication
+
+let ptr_async_add_exclusive_publication ?finalise () =
+  a ?finalise async_add_exclusive_publication
