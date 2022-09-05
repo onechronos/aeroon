c47627243e1960ec32e9dcc698c4ef1ebebc00a7
Mika Illouz <mika@illouz.net>
2022-09-05 11:54:38 -0700

    adapted pubsub to new ctypes-less low-level API

--- test/pubsub.ml
+++ test/pubsub.ml
@@ -1,138 +1,107 @@
-open Aeron
-open Ctypes
-open C.Functions
+open Aeron.Raw
 
-let channel = "aeron:udp?endpoint=localhost:20121"
-let stream_id = 1001l
+let uri = "aeron:udp?endpoint=localhost:20121"
 
-
-let s_of_i  = Unsigned.Size_t.of_int
-let s_to_i  = Unsigned.Size_t.to_int
+let stream_id = 1001
 
 let pr = Printf.printf
 
 let context_and_client () =
-  let ctx =
-    let p_context = Alloc.ptr_context () in
-    let err = context_init p_context in
-    assert (err = 0);
-    !@ p_context
-  in
-
-  let client =
-    let p_client = Alloc.ptr_client () in
-    let err = init p_client ctx in
-    assert (err = 0);
-    !@ p_client
-  in
-
-  let err = start client in
-  assert (err = 0);
+  let ctx = context_init () in
+  let client = init ctx in
+  start client;
   ctx, client
 
+let cleanup ctx client =
+  close client;
+  context_close ctx
 
 let subscribe () =
   let ctx, client = context_and_client () in
-
-  let async =
-    let p_async = Alloc.ptr_async_add_subscription () in
-    let err = async_add_subscription p_async client channel stream_id None null None null in
-    assert (err >= 0);
-    !@ p_async
-  in
-
-  let subscription =
-    let p_subscription = Alloc.ptr_subscription () in
-    let rec poll () =
-      match async_add_subscription_poll p_subscription async with
-      | 1 -> !@ p_subscription
-      | 0 -> poll ()
-      | _ -> assert false
-    in
-    poll ()
-  in
-
-  let fragment_handler =
-    fun _ buf size _header ->
-      let message = string_from_ptr buf ~length:(s_to_i size) in
-      Printf.printf "received: %s\n%!" message
-  in
-
-  let fragment_assembler =
-    let p_fragment_assembler = Alloc.ptr_fragment_assembler () in
-    let err = fragment_assembler_create p_fragment_assembler fragment_handler null in
-    assert (err = 0);
-    !@ p_fragment_assembler
-  in
-
-  let rec poll () =
-    let num_fragments_read =
-      subscription_poll
-        subscription
-        fragment_assembler_handler
-        fragment_assembler
-        (s_of_i 10)
-    in
-    if num_fragments_read < 0 then
-      pr "subscribe error: %s\n%!" (errmsg ())
-    else (
-      main_idle_strategy client num_fragments_read;
+  match async_add_subscription client uri stream_id None None with
+  | None -> failwith "failed to get sub async"
+  | Some async ->
+    let subscription =
+      let rec poll () =
+        match async_add_subscription_poll async with
+        | Error -> failwith "async_add_subscription_poll error"
+        | TryAgain ->
+          Unix.sleep 1;
+          poll ()
+        | Ok subscription -> subscription
+      in
       poll ()
-    )
-  in
+    in
 
-  poll ();
+    let fragment_handler msg = Printf.printf "received: %s\n%!" msg in
+    Callback.register "fh" fragment_handler;
 
-  print_endline "close";
-  let _ = close client in
-  print_endline "context_close";
-  let _ = context_close ctx in
-  ()
+    let fragment_assembler =
+      match fragment_assembler_create fragment_handler with
+      | Some fragment_assembler -> fragment_assembler
+      | None -> failwith "failed to create fragment assembler"
+    in
+
+    let rec poll : unit -> unit =
+     fun () ->
+      match subscription_poll subscription fragment_assembler 10 with
+      | Some num_frags_read ->
+        if num_frags_read > 0 then
+          Printf.printf "num fragments read: %d\n" num_frags_read;
+        idle_strategy_sleeping_idle 1_000_000 num_frags_read;
+
+        poll ()
+      | None ->
+        Printf.printf "subscription_poll error: [%d]%s\n" (errcode ())
+          (errmsg ());
+        exit 1
+    in
+    poll ();
+    cleanup ctx client
 
 let publish () =
   let ctx, client = context_and_client () in
 
-  let async =
-    let p_async = Alloc.ptr_async_add_publication () in
-    let err = async_add_publication p_async client channel stream_id in
-    assert (err >= 0);
-    !@ p_async
-  in
-
   let publication =
-    let p_publication = Alloc.ptr_publication () in
+    let async = async_add_publication client uri stream_id in
     let rec poll () =
-      match async_add_publication_poll p_publication async with
-      | 1 -> !@ p_publication
-      | 0 -> poll ()
-      | _ -> assert false
+      match async_add_publication_poll async with
+      | Ok pub -> pub
+      | TryAgain -> poll ()
+      | Error -> failwith "pub error"
     in
     poll ()
   in
 
-  let msg = "This is a test of the emergency broadcast system. This is \
-             only a test." in
+  let msg =
+    "This is a test of the emergency broadcast system. This is only a test."
+  in
+
+  let string_of_publication_error = function
+    | Not_connected -> "not connected"
+    | Back_pressured -> "back pressured"
+    | Admin_action -> "admin action"
+    | Closed -> "closed"
+    | Max_position_exceeded -> "max position exceeded"
+    | Error -> "error"
+  in
 
   let rec pub n i =
-    if i < n then
+    if i < n then (
       let msg = Printf.sprintf "[%d] %s" i msg in
-      let buffer_size = s_of_i (String.length msg) in
-      let status = publication_offer publication
-          msg buffer_size None null in
-      pr "status=%Ld\n%!" status;
-      Unix.sleep 1;
-      pub n (i + 1)
-    else
-      ()
+      match publication_offer publication msg with
+      | Ok position ->
+        pr "publication position=%d\n%!" position;
+        Unix.sleepf 1e-6;
+        pub n (i + 1)
+      | Error code ->
+        Printf.printf "exclusive publication offer error=%s\n"
+          (string_of_publication_error code);
+        exit 1
+    )
   in
-  pub 100 0;
-
-  print_endline "close";
-  let _ = close client in
-  print_endline "context_close";
-  let _ = context_close ctx in
-  ()
-
+  pub 1_000_000 0;
+  cleanup ctx client
 
 let _ =
   let role = Sys.argv.(1) in
