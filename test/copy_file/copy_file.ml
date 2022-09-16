f894faceef3d238be768030ae442a8eada5d296a
Simon Cruanes <simon.cruanes.2007@m4x.org>
2022-09-16 16:28:41 -0400

    test: add zmq to copy_file, for comparison

--- test/copy_file/copy_file.ml
+++ test/copy_file/copy_file.ml
@@ -12,7 +12,8 @@ open struct
   let unwrap_ ~err_to_string = function
     | Ok x -> x
     | Error e ->
-      Log.err (fun k -> k "error: %s" (err_to_string e));
+      let bt = Printexc.get_backtrace () in
+      Log.err (fun k -> k "error: %s\n%s" (err_to_string e) bt);
       failwith "an error occurred"
 
   let unwrap_str_ x = unwrap_ ~err_to_string:Fun.id x
@@ -40,100 +41,189 @@ module Chunk = struct
   let make c = "c" ^ c
 end
 
-let canal ~port () : A.canal =
-  let uri = spf "aeron:udp?endpoint=localhost:%d" port in
-  { A.uri; stream_id = 1 }
+module With_aeron = struct
+  let canal ~port () : A.canal =
+    let uri = spf "aeron:udp?endpoint=localhost:%d" port in
+    { A.uri; stream_id = 1 }
 
-(* read input files *)
-let t_read ~(client : A.Client.t) ~port ~inputs () : unit =
-  let canal = canal ~port () in
-  let@ pub = A.Publication.with_ client canal in
-  let pub = unwrap_str_ pub in
+  (* read input files *)
+  let t_read ~(client : A.Client.t) ~port ~inputs () : unit =
+    let canal = canal ~port () in
+    let@ pub = A.Publication.with_ client canal in
+    let pub = unwrap_str_ pub in
 
-  let read1 file =
-    Log.info (fun k -> k "reading file %S" file);
-    let n_chunks = ref 0 in
+    let read1 file =
+      Log.info (fun k -> k "reading file %S" file);
+      let n_chunks = ref 0 in
 
-    let@ ic = CCIO.with_in file in
+      let@ ic = CCIO.with_in file in
 
-    CCIO.read_chunks_iter ~size:200 ic (fun chunk ->
-        incr n_chunks;
-        let chunk = Chunk.make chunk in
-        (* publish *)
-        ignore (A.Publication.offer pub chunk |> unwrap_pub_err_ : int));
+      CCIO.read_chunks_iter ~size:200 ic (fun chunk ->
+          incr n_chunks;
+          let chunk = Chunk.make chunk in
+          (* publish *)
+          ignore (A.Publication.offer pub chunk |> unwrap_pub_err_ : int));
 
-    Log.debug (fun k -> k "sent %d chunks for %S" !n_chunks file)
-  in
-  List.iter read1 inputs;
-  (* now close *)
-  Log.debug (fun k -> k "send EOF");
-  ignore (A.Publication.offer pub Chunk.eof |> unwrap_pub_err_ : int);
-  ()
-
-(* write into file *)
-let t_write ~(client : A.Client.t) ~port ~into ~n_written () : unit =
-  let canal = canal ~port () in
-  let@ sub = A.Subscription.with_ client canal in
-  let sub = unwrap_str_ sub in
-
-  let@ oc = CCIO.with_out into in
-
-  let continue = ref true in
-  let on_chunk c =
-    match Chunk.view c with
-    | Chunk.Eof ->
-      Log.info (fun k -> k "received EOF chunk");
-      continue := false
-    | Chunk.C s ->
-      ignore (Atomic.fetch_and_add n_written (String.length s) : int);
-      output_string oc s
-  in
-
-  let assembler =
-    A.Subscription.create_assembler on_chunk |> unwrap_opt_ "message assembler"
-  in
-  while !continue do
-    let n_read =
-      A.Subscription.poll sub assembler 10 |> unwrap_opt_ "poll subscription"
+      Log.debug (fun k -> k "sent %d chunks for %S" !n_chunks file)
     in
-    A.IdleStrategy.sleeping 5_000 n_read
-  done;
-  ()
+    List.iter read1 inputs;
+    (* now close *)
+    Log.debug (fun k -> k "send EOF");
+    ignore (A.Publication.offer pub Chunk.eof |> unwrap_pub_err_ : int);
+    ()
 
-let copy ~inputs ~into ~port () : unit =
-  let time_start = Unix.gettimeofday () in
+  (* write into file *)
+  let t_write ~(client : A.Client.t) ~port ~into ~n_written () : unit =
+    let canal = canal ~port () in
+    let@ sub = A.Subscription.with_ client canal in
+    let sub = unwrap_str_ sub in
 
-  Log.info (fun k -> k "start client");
-  let@ ctx = A.Context.with_ in
-  let ctx = unwrap_opt_ "could not create ctx" ctx in
-  let@ client = A.Client.with_create_start ctx in
-  let client = unwrap_opt_ "could not start client" client in
+    let@ oc = CCIO.with_out into in
 
-  let n_written = Atomic.make 0 in
+    let continue = ref true in
+    let on_chunk c =
+      match Chunk.view c with
+      | Chunk.Eof ->
+        Log.info (fun k -> k "received EOF chunk");
+        continue := false
+      | Chunk.C s ->
+        ignore (Atomic.fetch_and_add n_written (String.length s) : int);
+        output_string oc s
+    in
 
-  let t_read = Thread.create (fun () -> t_read ~client ~port ~inputs ()) () in
-  let t_write =
-    Thread.create (fun () -> t_write ~client ~port ~into ~n_written ()) ()
-  in
+    let assembler =
+      A.Subscription.create_assembler on_chunk
+      |> unwrap_opt_ "message assembler"
+    in
+    while !continue do
+      let n_read =
+        A.Subscription.poll sub assembler 10 |> unwrap_opt_ "poll subscription"
+      in
+      A.IdleStrategy.sleeping 5_000 n_read
+    done;
+    ()
 
-  Thread.join t_read;
-  Thread.join t_write;
+  let copy ~inputs ~into ~port () : unit =
+    let time_start = Unix.gettimeofday () in
 
-  let time_stop = Unix.gettimeofday () in
-  Log.info (fun k ->
-      k "wrote %d bytes in %.4fs (%.2fMB/s)" (Atomic.get n_written)
-        (time_stop -. time_start)
-        (float (Atomic.get n_written) /. 1e6 /. (time_stop -. time_start)));
-  Log.info (fun k -> k "all done, exiting");
-  ()
+    Log.info (fun k -> k "start client");
+    let@ ctx = A.Context.with_ in
+    let ctx = unwrap_opt_ "could not create ctx" ctx in
+    let@ client = A.Client.with_create_start ctx in
+    let client = unwrap_opt_ "could not start client" client in
+
+    let n_written = Atomic.make 0 in
+
+    let t_read = Thread.create (fun () -> t_read ~client ~port ~inputs ()) () in
+    let t_write =
+      Thread.create (fun () -> t_write ~client ~port ~into ~n_written ()) ()
+    in
+
+    Thread.join t_read;
+    Thread.join t_write;
+
+    let time_stop = Unix.gettimeofday () in
+    Log.info (fun k ->
+        k "wrote %d bytes in %.4fs (%.2fMB/s)" (Atomic.get n_written)
+          (time_stop -. time_start)
+          (float (Atomic.get n_written) /. 1e6 /. (time_stop -. time_start)));
+    Log.info (fun k -> k "all done, exiting");
+    ()
+end
+
+module With_zmq = struct
+  let uri = "inproc:///foo"
+
+  (* read input files *)
+  let t_read ~(ctx : Zmq.Context.t) ~inputs () : unit =
+    let sock = Zmq.Socket.create ctx Zmq.Socket.pub in
+    Zmq.Socket.set_send_high_water_mark sock 2_000_000;
+    let@ () = Fun.protect ~finally:(fun () -> Zmq.Socket.close sock) in
+    Zmq.Socket.connect sock uri;
+
+    Thread.delay 0.05;
+
+    let read1 file =
+      Log.info (fun k -> k "reading file %S" file);
+      let n_chunks = ref 0 in
+
+      let@ ic = CCIO.with_in file in
+
+      CCIO.read_chunks_iter ~size:200 ic (fun chunk ->
+          incr n_chunks;
+          let chunk = Chunk.make chunk in
+          (* publish *)
+          Zmq.Socket.send ~block:true sock chunk);
+
+      Log.debug (fun k -> k "sent %d chunks for %S" !n_chunks file)
+    in
+    List.iter read1 inputs;
+    (* now close *)
+    Log.debug (fun k -> k "send EOF");
+    Zmq.Socket.send ~block:true sock Chunk.eof;
+    ()
+
+  (* write into file *)
+  let t_write ~(ctx : Zmq.Context.t) ~into ~n_written () : unit =
+    let sock = Zmq.Socket.create ctx Zmq.Socket.sub in
+    let@ () = Fun.protect ~finally:(fun () -> Zmq.Socket.close sock) in
+    Zmq.Socket.bind sock uri;
+    Zmq.Socket.subscribe sock "";
+
+    let@ oc = CCIO.with_out into in
+
+    let continue = ref true in
+    let on_chunk c =
+      match Chunk.view c with
+      | Chunk.Eof ->
+        Log.info (fun k -> k "received EOF chunk");
+        continue := false
+      | Chunk.C s ->
+        ignore (Atomic.fetch_and_add n_written (String.length s) : int);
+        output_string oc s
+    in
+
+    while !continue do
+      let msg = Zmq.Socket.recv ~block:true sock in
+      on_chunk msg
+    done;
+    ()
+
+  let copy ~inputs ~into ~port:_ () : unit =
+    let time_start = Unix.gettimeofday () in
+
+    let ctx = Zmq.Context.create () in
+    let@ () = Fun.protect ~finally:(fun () -> Zmq.Context.terminate ctx) in
+    let n_written = Atomic.make 0 in
+
+    let t_read = Thread.create (fun () -> t_read ~ctx ~inputs ()) () in
+    let t_write =
+      Thread.create (fun () -> t_write ~ctx ~into ~n_written ()) ()
+    in
+
+    Thread.join t_read;
+    Thread.join t_write;
+
+    let time_stop = Unix.gettimeofday () in
+    Log.info (fun k ->
+        k "wrote %d bytes in %.4fs (%.2fMB/s)" (Atomic.get n_written)
+          (time_stop -. time_start)
+          (float (Atomic.get n_written) /. 1e6 /. (time_stop -. time_start)));
+    Log.info (fun k -> k "all done, exiting");
+    ()
+end
 
 let () =
+  Printexc.record_backtrace true;
   let debug = ref false in
   let files = ref [] in
   let port = ref 12456 in
+  let zmq = ref false in
   let opts =
     [
-      "-d", Arg.Set debug, " enable debug"; "-p", Arg.Set_int port, " set port";
+      "-d", Arg.Set debug, " enable debug";
+      "-p", Arg.Set_int port, " set port";
+      "--zmq", Arg.Set zmq, " use zmq";
     ]
     |> Arg.align
   in
@@ -149,5 +239,8 @@ let () =
   match !files with
   | into :: (_ :: _ as inputs) ->
     let inputs = List.rev inputs in
-    copy ~inputs ~into ~port:!port ()
+    if !zmq then
+      With_zmq.copy ~inputs ~into ~port:!port ()
+    else
+      With_aeron.copy ~inputs ~into ~port:!port ()
   | _ -> failwith "require at least 2 arguments"
